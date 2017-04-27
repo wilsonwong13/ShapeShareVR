779e58b1fe63d61566fb6dc139e590e12723b85a
Wilson Wong <wilsonwong13@gmail.com>
2017-04-27 14:39:31 -0400

    howdy

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ app.get('/', function(req, res, next) {
 	res.sendFile(__dirname + '/public/index.html')
 });
 
-var server = app.listen(process.env.PORT)
+var server = app.listen(1337, function () {
+    console.log('The server is listening on port 1337!');
+});
 
 var socketio = require('socket.io')
 
