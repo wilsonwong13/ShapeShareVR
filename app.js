7b30081b0805262e7b362c617d7cb6f482e773e4
Wilson Wong <wilsonwong13@gmail.com>
2017-04-27 11:38:20 -0400

    howdy

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/', function(req, res, next) {
 	res.sendFile(__dirname + '/public/index.html')
 });
-
 
 app.listen(process.env.PORT)
