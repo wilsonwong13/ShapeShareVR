6df83285dd161b1cf1496abc1473b084ce9f24a6
Wilson Wong <wilsonwong13@gmail.com>
2017-04-27 14:25:07 -0400

    howdy

--- app.js
+++ app.js
@@ -8,17 +8,3 @@ app.get('/', function(req, res, next) {
 });
 
 app.listen(process.env.PORT)
-
-
-var socketio = require('socket.io')
-
-
-var io = socketio(server);
-
-io.on('connection', function(socket) {
-	console.log('A user connected')
-
-	socket.on('disconnect', function() {
-		console.log('A user disconnect')
-	})
-})
