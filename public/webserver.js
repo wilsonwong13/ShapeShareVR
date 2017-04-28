470c6811796e7730843d5c9b6df61f27a3b3f93a
Wilson Wong <wilsonwong13@gmail.com>
2017-04-28 15:58:42 -0400

    howdy

--- public/webserver.js
+++ public/webserver.js
@@ -2,3 +2,7 @@ var socket = io(window.location.origin);
 
 socket.on('personalTest', function(data){console.log(data)})
 socket.on('message', function(data) {console.log(data)})
+
+function test() {
+	socket.broadcast.emit('personalTest','this is a test')
+}
