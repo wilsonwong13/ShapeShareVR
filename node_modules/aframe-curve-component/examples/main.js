b8d7d7782577d6d054331316351c6aebc648d394
Wilson Wong <wilsonwong13@gmail.com>
2017-04-29 01:30:42 -0400

    howyd

---
+++ node_modules/aframe-curve-component/examples/main.js
@@ -0,0 +1,2 @@
+//require('aframe'); --> Currently Master is required because: https://github.com/aframevr/aframe/commit/da02c7c5b2319e6923b6d5d3e49a2f3940763bf2#diff-dc3043915bc0e1dc643b7fa488e01c0e, will be added back with new release.
+require('../index.js');
